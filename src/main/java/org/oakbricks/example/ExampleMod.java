c0173637823c6cc4cdc257fd20f506e0f5c057d2
OakBricks <andycruzfranco@gmail.com>
2021-09-20 17:21:13 -0500

    works

--- src/main/java/org/oakbricks/example/ExampleMod.java
+++ src/main/java/org/oakbricks/example/ExampleMod.java
@@ -1,13 +1,15 @@
 package org.oakbricks.example;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ExampleMod implements ModInitializer {
 
-	private static Logger LOGGER = new LogManager("Example Mod");
+	private static Logger LOGGER = LogManager.getLogger("Example Mod");
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric World!")
+		LOGGER.info("Hello Fabric World!");
 	}
 }
